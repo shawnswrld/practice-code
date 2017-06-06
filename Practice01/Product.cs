52542f8ac4ddb4bb9f2154afd44cddacb7ad9486
shawnswrld <shawnswrld@gmail.com>
2017-06-06 19:24:39 -0400

    shawn

--- Practice01/Product.cs
+++ Practice01/Product.cs
@@ -3,3 +3,14 @@
 // Add properties for ProductId (should be a number type), Name, Price, Description
 //
 // Add a method named DisplayInfo that returns void and does a console ouput to show the Product's property info
+class Product
+{
+    public int Name { get; set; }
+    public int Price { get; set; }
+    public int Description { get; set; }
+    public int ProductId { get; set; }
+   
+    System.Console.DisplayInfo( Name+" "+Price+" "+Description+" "+ProductId);
+            }
+        
+}
